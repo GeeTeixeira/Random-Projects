d12d161978563f5525a00fca34fa779a67cec7c0
GeeTeixeira <39841512+GeeTeixeira@users.noreply.github.com>
2019-05-30 16:54:52 -0400

    class polynomial and linkedList
    Created the classes to store the expression in the class Polynomial and a linked list for storing all of them. Declared helper methods, findX and splitExp, to break down the expression into components that will be usable in the pascal's triangle.

--- polynomialCalculator.py
+++ polynomialCalculator.py
@@ -1,3 +1,25 @@
+class polynomial:
+    self.expression = None
+    self.degree = 0
+    def __init__(self, exp , deg):
+        self.expression = exp
+        self.degree = deg
+    def toString(self):
+        print(self.expression+"^"+self.degree)
+class polynomialLinkedList:
+    self.head = None
+    self.next = None
+    def __init__(self,exp):
+        self.head = exp
+    def setNext(self,nextNode):
+        self.next = nextNode
+    def toString(self):
+        pointer = self.head
+        while(pointer != None):
+            print(pointer.toString())
+            pointer = pointer.next
+
+    
 def generateTriangle(level):
   x = 0
   triangle = []
@@ -18,4 +40,31 @@ def generateTriangle(level):
     x+=1
   return triangle
 
-def polyCalc(pTriangle):
+def splitExp(exp):
+    if("-" in exp):
+        vals = exp.split("-")
+    if("+" in exp):
+        vals = exp.split("+")
+    vals[0] = vals[0].lstrip()
+    vals[0] = vals[0].rstrip()
+    vals[1] = vals[1].lstrip()
+    vals[1] = vals[1].rstrip()
+    return vals
+
+def findX(vals):
+    if(vals[0] == "x"):
+        return True
+    return False
+
+def polyCalc(pTriangle, expression):
+    degree = expression.degree
+    exp = expression.expression
+    triangle = generateTriangle(degree) # create pascal's triangle
+    vals = splitExp(exp) #split the expression into 2 terms x-2 into x,2
+    boolean = findX(vals) #find if X is the first term 
+    xDegree = degree
+    intDegree = 0
+    intVal = 0
+    while(xDegree >= 0 and intDegree >= degree):
+        
+    
